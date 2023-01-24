813fa50a3d2598a79b1e509c670d7b54481e53af
Luka <107890569+luqaben@users.noreply.github.com>
2023-01-24 12:30:46 +0200

    Päivitetty user.controller.js
    Word sivun 14 mukaiset muutokset

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,8 +1,38 @@
+import db from '../app.js';
+
 const registerUser = (req, res) => {
-    console.log(req.body);
-    res.render('register', {
-        message: " Lomake lähetetty!"
-    });
+    
+    // const kayttaja_tunnus = req.body.kayttaja_tunnus;
+    // const kayttaja_sahkoposti = req.body.kayttaja_sahkoposti;
+    // const kayttaja_salasana = req.body.kayttaja_salasana;
+    // const kayttaja_salasana_varmistus = req.body.kayttaja_salasana_varmistus;
+
+    const {
+        kayttaja_tunnus,
+        kayttaja_sahkoposti,
+        kayttaja_salasana,
+        kayttaja_salasana_varmistus,
+    } = req.body;
+
+    db.query(
+        "SELECT kayttaja_sahkoposti FROM kayttajat WHERE kayttaja_sahkoposti = ?",
+        
+        [kayttaja_sahkoposti],
+        (error, results) => {
+            if (error) {
+                console.log("Error in query: " + error);
+            }
+            if (results.length > 0) {
+                return res.render("register", {
+                    message: "Sähköposti on jo käytössä!",
+                });
+            } else if (kayttaja_salasana !== kayttaja_salasana_varmistus) {
+                return res.render("register", {
+                    message: "Salasanat eivät täsmänneet!",
+                });
+            }
+        }
+    );
 }
 
 export default registerUser;
